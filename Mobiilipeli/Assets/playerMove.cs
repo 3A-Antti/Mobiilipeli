239c97a89f7687704e114fcbc5dc1dc882b6f749
Julianteri <julius.toivola@opiskelija.lhkk.fi>
2023-03-09 12:09:11 +0200

    kosketusnäppäimet toiminnassa
    näytöllä on kaksi violettia nappia (muutetaan myöhemmin läpinäkyväks), jotka ohjaa alusta vasemmalle ja oikealle. Scriptiä on muokattu aikasemmasta aika paljon.

--- Mobiilipeli/Assets/playerMove.cs
+++ Mobiilipeli/Assets/playerMove.cs
@@ -6,6 +6,34 @@ public class playerMove : MonoBehaviour
 {
     float speed   = 5f;
     float r_speed = 90;
+    float z; 
+
+    public bool L_isHeldDown = false;
+    public bool R_isHeldDown = false;
+
+    public void L_onPress ()
+    {
+        L_isHeldDown = true;
+        //Debug.Log(L_isHeldDown);
+    }
+ 
+    public void L_onRelease ()
+    {
+        L_isHeldDown = false;
+        //Debug.Log(L_isHeldDown);
+    }
+
+    public void R_onPress ()
+    {
+        R_isHeldDown = true;
+        //Debug.Log(R_isHeldDown);
+    }
+ 
+    public void R_onRelease ()
+    {
+        R_isHeldDown = false;
+        //Debug.Log(R_isHeldDown);
+    }
 
     void Update()
     {
@@ -18,20 +46,18 @@ public class playerMove : MonoBehaviour
 
         float z = Input.GetAxis("Horizontal") * r_speed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.A) && !(Input.GetKey(KeyCode.D)))
+        
+
+        if (L_isHeldDown == true)
         {
             //transform.position += transform.up * Time.deltaTime * speed;
-            transform.Rotate(0, 0, z);     // tässä kohtaa alus/pelaaja kääntyy
-            //Debug.Log(z);
-            //Debug.Log("a"); 
+            transform.Rotate(0, 0, 0.1f);     
         }
 
-        if (Input.GetKey(KeyCode.D)  && !(Input.GetKey(KeyCode.A)))
+        if (R_isHeldDown == true)
         {
             //transform.position += transform.up * Time.deltaTime * speed;
-            transform.Rotate(0, 0, z);     
-            //Debug.Log(z);
-            //Debug.Log("d");
+            transform.Rotate(0, 0, -0.1f);     
         }
 
         if (Input.GetKey(KeyCode.S) && speed > 0)
@@ -44,4 +70,17 @@ public class playerMove : MonoBehaviour
             speed = speed + 0.005f;
         }
     }
+
+    public void moveLeft() 
+    {
+        transform.Rotate(0, 0, 0.1f);     // tässä kohtaa alus/pelaaja kääntyy
+
+        //z = Input.GetAxis("Horizontal") * r_speed * Time.deltaTime;
+        //Debug.Log("moveLeft() toimii");
+        //transform.position += transform.up * Time.deltaTime * speed;
+        //Debug.Log(z);
+        //Debug.Log("a"); 
+    }
+
+
 }
