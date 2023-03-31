e648f9fb2bc20d96e621b02375f9c89fee8ece6b
waltcoder2 <105769982+waltcoder2@users.noreply.github.com>
2023-03-31 10:16:30 +0300

    json

---
+++ Mobiilipeli/Assets/savedata.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class savedata : MonoBehaviour
+{
+    public scoreboard Scoreboard = new scoreboard();
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S)){
+            SaveToJson();
+        }
+        if(Input.GetKeyDown(KeyCode.L)){
+            LoadFromJson();
+        }
+    }
+
+    public void SaveToJson()
+    {
+        string inventoryData = JsonUtility.ToJson(Scoreboard);
+        string filePath = Application.persistentDataPath+"/InventoryData.json";
+        Debug.Log(filePath);
+        System.IO.File.WriteAllText(filePath, inventoryData);
+        Debug.Log("vittu saatana");
+    }
+    public void LoadFromJson(){
+      string filePath = Application.persistentDataPath+"/InventoryData.json";
+      string inventoryData = System.IO.File.ReadAllText(filePath);
+
+      Scoreboard = JsonUtility.FromJson<scoreboard>(inventoryData);
+      Debug.Log("ranskalaiset syödään ketsupilla");  
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class scoreboard
+{
+    public int score;
+    public bool levelC;
+    public List<Items> items = new List<Items>();
+}
+
+[System.Serializable]
+public class Items
+{
+    public string name;
+    public string desc;
+}
